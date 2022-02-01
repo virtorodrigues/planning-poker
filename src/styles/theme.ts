f8cdc419117da765493f55d5b93dfc8acd0f7732
vitor <santos_vitao@hotmail.com>
2022-02-01 19:38:55 -0300

    create index page

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+    500: '#2a69ac',
+    400: '#2a69ac',
+    300: '#2a69ac',
+    200: '#2a69ac',
+    100: '#2a69ac',
+  },
+}
+
+export const theme = extendTheme({ colors })

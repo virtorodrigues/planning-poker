bb4b963dbaf63ca1b9808d780c2215b078d5ec61
vitor <santos_vitao@hotmail.com>
2022-02-03 23:15:54 -0300

    add task title + text area to add task title

---
+++ src/components/TaskTitle.tsx
@@ -0,0 +1,9 @@
+import { Heading } from "@chakra-ui/react";
+
+export function TaskTitle() {
+  return (
+    <Heading>
+      Titulo da task
+    </Heading>
+  )
+}
--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,18 +1,34 @@
-import { Flex } from '@chakra-ui/react'
+import { Button, Flex, Stack, Textarea } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import { CardUser } from '../components/CardUser';
 import { ListOfCards } from '../components/ListOfCards';
+import { TaskTitle } from '../components/TaskTitle';
 
-const Landing: NextPage = () => {
-
+const Game: NextPage = () => {
   return (
-    <Flex h="100vh" direction="column" >
+    <>
+      <Flex direction="column" justify="center" align="center" my="120px">
+        <Flex justify="center" align="center" direction="column" flex={1}>
 
-      <CardUser score={54} />
+          <Flex mb={100}>
+            <TaskTitle />
+          </Flex>
+
+
+          <Stack maxWidth={780} w={780} spacing={5} align="end">
+            <Textarea maxHeight={200} maxWidth={780} placeholder="Digite o título da tarefa...." />
+            <Button width={100} colorScheme="blue">Confirmar</Button>
+          </Stack>
+
+
+          <CardUser score={54} />
+        </Flex>
+
+      </Flex>
 
       <ListOfCards />
-    </Flex>
+    </>
   )
 }
 
-export default Landing
+export default Game
